db27bb49d06f3023855fa87631e18140fabd4cf7
Bartosz Hliwa <bartosz.hliwa@gmail.com>
2017-01-26 19:07:54 +0100

    GetFiles on main page

---
+++ src/Task3/Startup.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Task3
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            Task2.Task2 files = new Task2.Task2();
+            files.GetFiles(Directory.GetCurrentDirectory());
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var dir in files.Dirs)
+            {
+                sb.Append("<b>|-- " + dir.Name + "</b></br>");
+            }
+
+            foreach (var file in files.Files)
+            {
+                sb.Append("|-- " + file.Name + "</br>");
+            }
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync(sb.ToString());
+            });
+        }
+    }
+}
